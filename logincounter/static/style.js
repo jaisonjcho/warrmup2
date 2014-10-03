2e9fc4a8996ddc35cb2f727f4b0c6279ecca5202
jaisonjcho <rio1223@berkeley.edu>
2014-10-03 06:37:37 -0700

    frontend

---
+++ logincounter/static/style.js
@@ -0,0 +1,111 @@
+$(document).ready(function() {
+                  show_login_page("Please sign in or create a new user")
+                  //});
+
+function show_login_page(message) {
+    if(! message) message = "Please sign in or create a new user";
+    $('#welcome-page').hide()
+    $('#login-username').val("")
+    $('#login-password').val("")
+    $('#login-message').html(message)
+    $('#login-page').show()
+}
+
+function show_welcome_page(user, count) {
+    $('#login-page').hide();
+    $('#welcome-page').show();
+    $('#welcome-message').html("Welcome "+user+"<br>You have logged in "+count+" times.");
+}
+
+function handle_login_response(data, user) {
+    if( data.errCode > 0 ) {
+        c = data.count;
+        show_welcome_page(user, c);
+    } else {
+        if( debug_flag ) {
+            if( data.errCode != ERR_BAD_CREDENTIALS ) {
+                alert( 'Illegal error code encounted for this state');
+            }
+        }
+        show_login_page( get_message_for_errcode(data.errCode) );
+    }
+}
+
+function handle_add_user_response(data, user) {
+    if( data.errCode > 0 ) {
+        c = data.count;
+        show_welcome_page(user, c);
+    } else {
+        if( debug_flag ) {
+            if( data.errCode != ERR_BAD_USERNAME && data.errCode != ERR_USER_EXISTS ) {
+                alert( 'Illegal error code encounted for this state');
+            }
+        }
+        show_login_page( get_message_for_errcode(data.errCode) );
+    }
+}
+
+// POST to /users/login -- log in user and increase count
+$('#login-button').click(function(e) {
+    e.preventDefault()
+    username = $('#login-username').val()
+    password = $('#login-password').val()
+    json_request("/users/login", { user: username, password: password }, function(data) { return handle_login_response(data, username); }, function(err) { alert('error occurred on request'); });
+                         
+    return false;
+});
+
+// POST to /users/add -- create new user
+$('#add-user-button').click(function() {
+                            username = $('#login-username').val()
+                            password = $('#login-password').val()
+                            json_request("/users/add", { user: username, password: password }, function(data) { return handle_add_user_response(data, username); }, function(err) {alert('error occurred on request'); });
+                            
+                            return false;
+                            });
+
+$('#logout-button').click(function() {
+                          show_login_page();
+                          
+                          return false;
+                          });
+
+/* do POST requests and handle responses */
+function json_request(page, dict, success, failure) {
+    $.ajax({
+           type: 'POST',
+           url: page,
+           data: JSON.stringify(dict),
+           contentType: "application/json",
+           dataType: "json",
+           success: success,
+           error: failure
+           });
+}
+
+debug_flag = false;
+
+ERR_BAD_CREDENTIALS = (-1);
+ERR_USER_EXISTS = (-2);
+ERR_BAD_USERNAME = (-3);
+ERR_BAD_PASSWORD  = (-4);
+
+
+function get_message_for_errcode(code) {
+    if( code == ERR_BAD_CREDENTIALS) {
+        return ("Invalid username and/or password. Please try again.");
+    } else if( code == ERR_BAD_USERNAME) {
+        return ("The username cannot be empty or over 128 characters. Please try again.");
+    } else if( code == ERR_USER_EXISTS) {
+        return ("This user name already exists. Please try again.");
+    } else if( code == ERR_BAD_PASSWORD) {
+        return ("The password should be at most 128 characters long. Please try again");
+    } else {
+        if( debug_flag ) { 
+            alert('Illegal error code encountered: ' + code); 
+        }
+        return ("Unknown error occured: " + code);
+    }
+}
+
+});
---
+++ logincounter/views.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django import forms
+from logincounter.models import User
+import json
+from django.views.decorators.csrf import csrf_exempt
+
+class UserForm(forms.Form):
+	MAX_PASSWORD_LENGTH = 128
+	MAX_USERNAME_LENGTH = 128
+	
+	user = forms.CharField(max_length=MAX_PASSWORD_LENGTH, required=True)
+	password = forms.CharField(max_length=MAX_USERNAME_LENGTH)
+
+@csrf_exempt
+def home(request):
+	form = UserForm()
+	return render(request, 'index.html', {'form': form})
+
+@csrf_exempt	
+def login(request):
+	if request.method == 'POST':
+		data = json.loads(request.body.encode(encoding='UTF-8'))
+		return HttpResponse(json.dumps(User.login(data)), content_type="application/json")
+
+@csrf_exempt      
+def add(request):
+	if request.method == 'POST':
+		data = json.loads(request.body.encode(encoding='UTF-8'))
+		return HttpResponse(json.dumps(User.add(data)), content_type="application/json")
+
+@csrf_exempt
+def resetFixture(request):
+	if request.method == 'POST':
+		return HttpResponse(json.dumps(User.resetFixture()), content_type="application/json")
+	
+import os
+import subprocess
+ 
+@csrf_exempt
+def unitTests(request):
+	if request.method == 'POST':
+		nrFailed = 0
+		totalTests = 0
+		output = subprocess.check_output("python manage.py test logincounter; exit 0", shell=True, stderr=subprocess.STDOUT)
+		lines = output.splitlines()
+		for line in lines:
+			if line.startswith("Ran "):
+				totalTests = int(line.split(" ")[1])
+			elif line.startswith("FAILED (failures="):
+				nrFailed = line.split("=")[1][0:-1]
+		return HttpResponse(json.dumps({'nrFailed': nrFailed, 'output': output, 'totalTests': totalTests}), content_type="application/json")
+
+	return HttpResponse(json.dumps({}), content_type="application/json")
